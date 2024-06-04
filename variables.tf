e94c6f6199b543ac22636e9f63d86fe834cb633f
andysa7 <andysa7777@gmail.com>
2024-06-04 02:58:48 +0000

    ok thanks

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+
+variable "name" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+
+variable "environment" {
+  type        = string
+  description = "(optional) describe your variable"
+}
